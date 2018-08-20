abb2c83276e6f6c90a4686ff02d4fde525da63f6
dsasmita <dangs.work@gmail.com>
2018-08-20 20:31:18 +0700

    Create list product from specified categoryId

--- Block/Hello.php
+++ Block/Hello.php
@@ -4,13 +4,32 @@ namespace Dangs\HelloModule\Block;
 
 class Hello extends \Magento\Framework\View\Element\Template
 {
-	public function __construct(\Magento\Framework\View\Element\Template\Context $context)
+	protected $_categoryFactory;
+
+	public function __construct(
+		\Magento\Framework\View\Element\Template\Context $context,
+		\Magento\Catalog\Model\CategoryFactory $categoryFactory
+	)
 	{
+		$this->_categoryFactory = $categoryFactory;
 		parent::__construct($context);
+	}
+
+	public function getCategory()
+	{
+	    $categoryId = 3;
+	    $category = $this->_categoryFactory->create()->load($categoryId);
+	    return $category;
+	}
+
+	public function getProductCollection()
+	{
+	     return $this->getCategory()->getProductCollection()->addAttributeToSelect('*')->setPageSize(10); 
 	}
 
 	public function returnHello()
 	{
-		return __('Hello Module');
+		$category = $this->getCategory();
+		return __('Product under category: '.$category->getName());
 	}
 }
