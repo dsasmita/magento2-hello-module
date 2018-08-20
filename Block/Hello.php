5b9a438e8188ef6a8439649dbb288d26830f5148
dsasmita <dangs.work@gmail.com>
2018-08-20 19:31:03 +0700

    Add block and layout

---
+++ Block/Hello.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Dangs\HelloModule\Block;
+
+class Hello extends \Magento\Framework\View\Element\Template
+{
+	public function __construct(\Magento\Framework\View\Element\Template\Context $context)
+	{
+		parent::__construct($context);
+	}
+
+	public function returnHello()
+	{
+		return __('Hello Module');
+	}
+}
--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -3,10 +3,18 @@ namespace Dangs\HelloModule\Controller\Index;
 
 class Index extends \Magento\Framework\App\Action\Action
 {
+	protected $_pageFactory;
+	
+	public function __construct(
+		\Magento\Framework\App\Action\Context $context,
+		\Magento\Framework\View\Result\PageFactory $pageFactory)
+	{
+		$this->_pageFactory = $pageFactory;
+		return parent::__construct($context);
+	}
+
 	public function execute()
 	{
-		echo '<h1>Hello Module</h1>';
-		echo "<p>congratulation you just create your first module!</p>";
-		exit;
+		return $this->_pageFactory->create();
 	}
 }
